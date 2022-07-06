f70c963c85ed16c93b247d9c877cc860b677fe8e
Ruslan <81380652+RuslanJunior2005@users.noreply.github.com>
2022-07-06 14:16:23 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,25 +3,14 @@
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
-
     document.body.classList.toggle('light-theme');
-
     document.body.classList.toggle('dark-theme');
 
     const className = document.body.className;
-
     if(className == "light-theme") {
-
         this.textContent = "Dark";
-
     } else {
-
-        this.textContent = "Light";
-
+        this.textContent = "Green";
     }
-
-    
-
     console.log('current class name: ' + className);
-
 });
